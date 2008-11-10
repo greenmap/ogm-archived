a3fe1ab98655d5095d4761d17bafa55b67fd27e7
Thomas Turnbull <thomasturnbull@users.noreply.github.com>
2008-11-10 21:51:03 +0000

    Deleting un-used custom garland theme

--- themes/custom/garland/gmap_views_maker_label.tpl.php
+++
@@ -1,6 +0,0 @@
-<?php
-  $marker_label = '';
-  foreach ($view->field as $field) {
-    $marker_label .= '<div class="gmapstyle-'. $field['field'] .'">'. views_theme_field('views_handle_field', $field['queryname'], $fields, $field, $entry, $view) .'</div>';
-  }
-  print $marker_label; ?>
