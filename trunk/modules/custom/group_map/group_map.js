45869cc91902b3ab6b2ec7b3e0f698584003ce82
thomasturnbull <thomasturnbull@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2008-12-09 19:59:08 +0000

    =adding js to hide the search box only when the key is loaded

--- trunk/modules/custom/group_map/group_map.js
+++ trunk/modules/custom/group_map/group_map.js
@@ -2,6 +2,14 @@
  * by tom_o_t - aka Thomas Turnbull for Green Map System
  * Javascript for the map 
  */
+ 
+/*
+* Use jquery to hide the main search box when the key block is loaded
+*/
+
+$(document).ready(function(){
+    $("#block-search-0").hide();
+});
 
 
 /**
