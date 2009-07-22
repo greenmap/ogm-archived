b0e1f9225010e1e73194453c071d4f5cb06f2703
Thomas Turnbull <thomasturnbull@users.noreply.github.com>
2009-07-22 04:00:24 +0000

    Creating zen subtheme for mobile theme

--- sites/all/themes/zen/STARTERKIT/theme-settings.php
+++ sites/all/themes/zen/STARTERKIT/theme-settings.php
@@ -13,10 +13,10 @@ include_once './' . drupal_get_path('theme', 'zen') . '/theme-settings.php';
  * @return
  *   A form array.
  */
-function STARTERKIT_settings($saved_settings) {
+function mobile_settings($saved_settings) {
 
   // Get the default values from the .info file.
-  $defaults = zen_theme_get_default_settings('STARTERKIT');
+  $defaults = zen_theme_get_default_settings('mobile');
 
   // Merge the saved variables and their default values.
   $settings = array_merge($defaults, $saved_settings);
