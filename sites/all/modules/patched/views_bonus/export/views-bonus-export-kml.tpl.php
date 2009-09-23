e9a00a103be16988b6487e8febb0a1a9c88cbb3c
mattc <mattc@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2009-09-23 21:45:25 +0000

    add primary icon to KML feed (issue #1721); move changes to testimonials view out of DB and into a file

--- sites/all/modules/patched/views_bonus/export/views-bonus-export-kml.tpl.php
+++ sites/all/modules/patched/views_bonus/export/views-bonus-export-kml.tpl.php
@@ -12,54 +12,95 @@
  * @ingroup views_templates
  */
 
+$hostname = 'http://'. check_plain($_SERVER["SERVER_NAME"]). base_path();
+
 // Short tags act bad below in the html so we print it here.
-print '<?xml version="1.0" encoding="UTF-8"?>';
-?>
+print '<?xml version="1.0" encoding="UTF-8"?'.'>'."\n";
+print '<kml xmlns="http://www.opengis.net/kml/2.2">'."\n";
 
-<?php
-print '<kml xmlns="http://www.opengis.net/kml/2.2">';
-?>
+print "<Document>\n";
 
-<Document>
-<?php foreach ($themed_rows as $count => $row): ?>
-  <Placemark>
-<?php foreach ($row as $field => $content):
+foreach ($themed_rows as $count => $row) {
+  foreach ($row as $field => $content) {
     $label = $header[$field] ? $header[$field] : $field;
-//skip output if the label is "extra"
-//this allows for multiple fields to be combined
-//via the views interface without printing
-//invalid elements to the kml file
-    if ($label=="extra") {
-   //do nothing
-    }else {
+    if ($label == "icon") {
+      $primary_term = NULL;
+      $primary_terms = taxonomy_get_term_by_name($content);
+      foreach($primary_terms as $possible_primary_term) {
+        if ($possible_primary_term->vid == 1) {
+          $primary_term = $possible_primary_term;
+        }
+      }
+      if ($primary_term) {
+        $url = $hostname."sites/default/files/kml_icons/".$primary_term->tid.".png";
+        print '
+<Style id="icon'.$primary_term->tid.'">
+  <IconStyle>
+    <Icon>
+      <href>'.$url.'</href>
+    </Icon>
+  </IconStyle>
+</Style>
+';
+      }
+    }
+  }
+}
 
-    $is_a_point="NO";
-    if ($label=="coordinates") {
-    	$is_a_point= "YES";
+foreach ($themed_rows as $count => $row) {
+  print "<Placemark>\n";
+  foreach ($row as $field => $content) {
+    $label = $header[$field] ? $header[$field] : $field;
+
+    //skip output if the label is "extra"
+    //this allows for multiple fields to be combined
+    //via the views interface without printing
+    //invalid elements to the kml file
+    if ($label == "extra") {
+     //do nothing
     }
-    if ($is_a_point=="YES") {
-    	print "<Point>";
+    else {
+
+      if ($label == "icon") {
+        $label = 'styleUrl';
+        $primary_term = NULL;
+        $primary_terms = taxonomy_get_term_by_name($content);
+        foreach($primary_terms as $possible_primary_term) {
+          if ($possible_primary_term->vid == 1) {
+            $primary_term = $possible_primary_term;
+          }
+        }
+        if ($primary_term) {
+          $content = "#icon".$primary_term->tid;
+        }
+      }
+
+      $is_a_point = "NO";
+      if ($label == "coordinates") {
+        $is_a_point = "YES";
+      }
+      if ($is_a_point == "YES") {
+        print "<Point>";
+      }
+      $is_description = "NO";
+      if ($label == "description") {
+        $is_description = "YES";
+      }
+      print "<". $label .">";
+      if ($is_description == "YES") {
+        print "<![CDATA[";
+      }
+      print $content;
+      if ($is_description == "YES") {
+        print "]]>";
+      }
+      print "</". $label.">";
+      if ($is_a_point == "YES") {
+        print "</Point>";
+      }
     }
-    $is_description="NO";
-    if ($label=="description") {
-    	$is_description="YES";
-    }
-    print "<". $label .">";
-    if ($is_description =="YES") {
-    	print "<![CDATA[";
-    }
-    print $content;
-    if ($is_description == "YES") {
-    	print "]]>";
-    }
-    print "</". $label.">";
-    if ($is_a_point=="YES") {
-    	print "</Point>";
-    }
-    }
-    ?>
-<?php endforeach; ?>
-  </Placemark>
-<?php endforeach; ?>
-</Document>
-</kml>
+  } // foreach
+  print "</Placemark>\n";
+} // foreach
+print "</Document>\n";
+print "</kml>\n";
