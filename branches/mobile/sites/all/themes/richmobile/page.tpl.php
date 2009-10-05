7383150c92e238965a295dc7c574b4599ea101f3
te <te@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2009-10-05 23:21:13 +0000

    file pulled off of dev site that te edited with out knowing what he was doing. adding iphone like theming to page.tpl

--- branches/mobile/sites/all/themes/richmobile/page.tpl.php
+++ branches/mobile/sites/all/themes/richmobile/page.tpl.php
@@ -1,96 +1,72 @@
 <?php
-// $Id: page.tpl.php,v 1.14.2.6 2009/02/13 16:28:33 johnalbin Exp $
 
-/**
- * @file page.tpl.php
- *
- * Theme implementation to display a single Drupal page.
- *
- * Available variables:
- *
- * General utility variables:
- * - $base_path: The base URL path of the Drupal installation. At the very
- *   least, this will always default to /.
- * - $css: An array of CSS files for the current page.
- * - $directory: The directory the theme is located in, e.g. themes/garland or
- *   themes/garland/minelli.
- * - $is_front: TRUE if the current page is the front page. Used to toggle the mission statement.
- * - $logged_in: TRUE if the user is registered and signed in.
- * - $is_admin: TRUE if the user has permission to access administration pages.
- *
- * Page metadata:
- * - $language: (object) The language the site is being displayed in.
- *   $language->language contains its textual representation.
- *   $language->dir contains the language direction. It will either be 'ltr' or 'rtl'.
- * - $head_title: A modified version of the page title, for use in the TITLE tag.
- * - $head: Markup for the HEAD section (including meta tags, keyword tags, and
- *   so on).
- * - $styles: Style tags necessary to import all CSS files for the page.
- * - $scripts: Script tags necessary to load the JavaScript files and settings
- *   for the page.
- * - $body_classes: A set of CSS classes for the BODY tag. This contains flags
- *   indicating the current layout (multiple columns, single column), the current
- *   path, whether the user is logged in, and so on.
- * - $body_classes_array: An array of the body classes. This is easier to
- *   manipulate then the string in $body_classes.
- *
- * Site identity:
- * - $front_page: The URL of the front page. Use this instead of $base_path,
- *   when linking to the front page. This includes the language domain or prefix.
- * - $logo: The path to the logo image, as defined in theme configuration.
- * - $site_name: The name of the site, empty when display has been disabled
- *   in theme settings.
- * - $site_slogan: The slogan of the site, empty when display has been disabled
- *   in theme settings.
- * - $mission: The text of the site mission, empty when display has been disabled
- *   in theme settings.
- *
- * Navigation:
- * - $search_box: HTML to display the search box, empty if search has been disabled.
- * - $primary_links (array): An array containing primary navigation links for the
- *   site, if they have been configured.
- * - $secondary_links (array): An array containing secondary navigation links for
- *   the site, if they have been configured.
- *
- * Page content (in order of occurrance in the default page.tpl.php):
- * - $left: The HTML for the left sidebar.
- *
- * - $breadcrumb: The breadcrumb trail for the current page.
- * - $title: The page title, for use in the actual HTML content.
- * - $help: Dynamic help text, mostly for admin pages.
- * - $messages: HTML for status and error messages. Should be displayed prominently.
- * - $tabs: Tabs linking to any sub-pages beneath the current page (e.g., the view
- *   and edit tabs when displaying a node).
- *
- * - $content: The main content of the current Drupal page.
- *
- * - $right: The HTML for the right sidebar.
- *
- * Footer/closing data:
- * - $feed_icons: A string of all feed icons for the current page.
- * - $footer_message: The footer message as defined in the admin settings.
- * - $footer : The footer region.
- * - $closure: Final closing markup from any modules that have altered the page.
- *   This variable should always be output last, after all other dynamic content.
- *
- * @see template_preprocess()
- * @see template_preprocess_page()
- * Use the above functions in template.php to add make additional variables available to this theme
- */
-?>
-<!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd"> 
-<html>
+?> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+	
+			<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; minimum-scale=1.0; user-scalable=0;" />
+		<link media="screen" href="http://greenmap.org/dev/ogm_miikka3/sites/all/themes/mobile/css/fixed.css" type= "text/css" rel="stylesheet" />
+		<script src="http://greenmap.org/dev/ogm_miikka3/sites/all/themes/mobile/js/fixed.js" type="text/javascript" charset="utf-8"></script>
+		<script src="http://greenmap.org/dev/ogm_miikka3/sites/all/themes/mobile/js/nav.js" type="text/javascript"></script>
+	
+				<script type="text/javascript">
+    var Drupal_base_path = '<?php print base_path()?>';
+  </script>
+  
+
+	</head>
+	
+					
+	<body>
+		<div id="header">
+		<div id="nav" onclick="javascript:showElement('navwin')">
+		Menu</div>
+			<span class="logo"><img src="http://greenmap.org/dev/ogm_miikka3/sites/all/themes/mobile/img/logo.png" height="38px">
+			</span>
+		</div>
+		
+		<div id="navwin" style="display:none">
+	<div id="dropsheet"></div>	
+	
+	<div class="wrapper">
+		<div id="nav-head">
+		
+			<div id="close-nav" onclick="javascript:showElement('navwin')">&nbsp;</div>
+		</div>
+		
+		<a class="row1 col1"></a>
+		<a class="row1 col2"></a>
+		<a class="row1 col3" href="http://greenmap.org/dev/ogm_miikka3/mobile" onclick="myEventTracker('NAV', 'Click', 'Home'); return true;"></a>
+		<a class="row2 col1"></a>
+		<a class="row2 col2" href="http://greenmap.org/dev/ogm_miikka3/sites/all/themes/mobile/explore.html" onclick="myEventTracker('NAV', 'Click', 'Home'); return true;"></a>
+		<a class="row2 col3" href="javascript:hideNAV();"></a>
+		<div style="display: block;">
+			<img src="http://greenmap.org/dev/ogm_miikka3/sites/all/themes/mobile/img/nav.png">
+		    	</div>		
+	</div>
+</div>
+		
+		
+		<div id="container">
+
 
 <head>
-  <title><?php print $head_title; ?></title>
+  <title>What's Green Nearby?</title>
   <?php print $head; ?>
-  <?php print $styles; // add new styles in richmobile.info ?>
-  <?php print $scripts; // add new scripts in richmobile.info ?>
+  <?php // print $styles; ?>
+  <?php // print $scripts; ?>
   <meta name = "viewport" content = "width = device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;">
-
+  <style>
+  body {
+    -webkit-text-size-adjust: none;
+    min-height: 402px;
+  }
+  </style>
 
   <script src="http://code.google.com/apis/gears/gears_init.js" type="text/javascript" charset="utf-8"></script> 
-	<script src="<?php print $base_path ?>sites/all/modules/custom/ogm_mobile/geo.js" type="text/javascript" charset="utf-8"></script> 
+	<script src="<?php print base_path() ?>sites/all/modules/custom/ogm_mobile/geo.js" type="text/javascript" charset="utf-8"></script> 
   
 </head>
 <body onLoad="setTimeout(scrollTo, 100, 0, 1);" >
@@ -102,7 +78,7 @@
       <div id="skip-to-nav"><a href="#navigation"><?php print t('Skip to Navigation'); ?></a></div>
     <?php endif; ?>
 
-    <div id="header"><div id="header-inner" class="clear-block">
+    <div id="header_php"><div id="header-inner" class="clear-block">
 
       <?php if ($logo || $site_name || $site_slogan): ?>
         <div id="logo-title">
@@ -160,7 +136,7 @@
           <div id="content-header">
             <?php print $breadcrumb; ?>
             <?php if ($title): ?>
-              <h1 class="title"><?php print $title; ?></h1>
+              <h1 class="title">What's Green Nearby?</h1>
             <?php endif; ?>
             <?php print $messages; ?>
             <?php if ($tabs): ?>
@@ -255,4 +231,19 @@
   <?php print $closure; ?>
 
 </body>
+
+
+		</div>
+	</body>
+   <?php print $closure; ?>
+   <script type="text/javascript">
+   var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+   document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+   </script>
+   <script type="text/javascript">
+   try{
+     var pageTracker = _gat._getTracker("UA-418876-6");
+     pageTracker._trackPageview();
+   } catch(err) {}</script>
+
 </html>
