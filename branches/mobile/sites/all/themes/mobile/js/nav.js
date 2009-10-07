824d450792dfcb9770d139641dcc230e82d18e33
thomasturnbull <thomasturnbull@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2009-10-07 00:04:58 +0000

    Deleting old js directory from mobile theme.

--- branches/mobile/sites/all/themes/mobile/js/nav.js
+++
@@ -1,9 +0,0 @@
-function showElement(navwin){
-var navwin = document.getElementById(navwin);
-if(navwin.style.display=="none"){
-navwin.style.display="block";
-menu.backgroundPosition="top";
-} else {
-navwin.style.display="none";
-}
-}
