2ffd56058235601126b07eca67ef8f2e7113a874
Thomas Turnbull <thomasturnbull@users.noreply.github.com>
2009-11-04 03:47:59 +0000

    mobile: theming mini-pager to remove raquo, and getting it inline instead of in list.

--- sites/all/themes/mobile/template.php
+++ sites/all/themes/mobile/template.php
@@ -132,6 +132,52 @@ function mobile_form_element($element, $value) {
   return $output;
 }
 
+function mobile_views_mini_pager($tags = array(), $limit = 10, $element = 0, $parameters = array(), $quantity = 9) {
+  global $pager_page_array, $pager_total;
+
+  // Calculate various markers within this pager piece:
+  // Middle is used to "center" pages around the current page.
+  $pager_middle = ceil($quantity / 2);
+  // current is the page we are currently paged to
+  $pager_current = $pager_page_array[$element] + 1;
+  // max is the maximum page number
+  $pager_max = $pager_total[$element];
+  // End of marker calculations.
+
+
+  $li_previous = theme('pager_previous', (isset($tags[1]) ? $tags[1] : t('<')), $limit, $element, 1, $parameters);
+  if (empty($li_previous)) {
+    $li_previous = "&nbsp;";
+  }
+
+  $li_next = theme('pager_next', (isset($tags[3]) ? $tags[3] : t('>')), $limit, $element, 1, $parameters);
+  if (empty($li_next)) {
+    $li_next = "&nbsp;";
+  }
+
+  if ($pager_total[$element] > 1) {
+    $items[] = array(
+      'class' => 'pager-previous',
+      'data' => $li_previous,
+    );
+
+    $items[] = array(
+      'class' => 'pager-current',
+      'data' => t('@current of @max', array('@current' => $pager_current, '@max' => $pager_max)),
+    );
+
+    $items[] = array(
+      'class' => 'pager-next',
+      'data' => $li_next,
+    );
+    // return theme('item_list', $items, NULL, 'ul', array('class' => 'pager'));
+    foreach ($items as $item) {
+      $output .= ' <span class="' . $item['class'] . '"> ' . $item['data'] . ' </span> ';
+    }
+    return $output;
+  }
+}
+
 /**
  * Override or insert variables into all templates.
  *
