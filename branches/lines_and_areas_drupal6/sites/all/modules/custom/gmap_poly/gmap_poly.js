d3737c87ad55f3559b18cbd4adf901451c340646
nat <nat@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2009-12-11 19:20:11 +0000

    Added, but disabled setting the center and zoom of widget map based on group settings. Changed the name of the widget map div.

--- branches/lines_and_areas_drupal6/sites/all/modules/custom/gmap_poly/gmap_poly.js
+++ branches/lines_and_areas_drupal6/sites/all/modules/custom/gmap_poly/gmap_poly.js
@@ -1,29 +1,37 @@
-     var kmltest = '<?xml version="1.0" encoding="UTF-8"?>\n' +
-             '<kml>\n' +
-             '<Document>\n' +
-             '<Placemark>' +
-             '<name>Test</name>' +
-             '<LineString>' +
-             '<coordinates>' +
-             '-74.00176048278809,40.724884598773755  -73.99309158325195,40.72085157020638  -73.99712562561035,40.71577741296778  -74.00588035583496,40.71779411151555\n' +
-             '</coordinates>' +
-             '</LineString>' +
-             '</Placemark>' +
-             '</Document>\n' +
-             '</kml>\n';
+var kmltest = '<?xml version="1.0" encoding="UTF-8"?>\n' +
+        '<kml>\n' +
+        '<Document>\n' +
+        '<Placemark>' +
+        '<name>Test</name>' +
+        '<LineString>' +
+        '<coordinates>' +
+        '-74.00176048278809,40.724884598773755  -73.99309158325195,40.72085157020638  -73.99712562561035,40.71577741296778  -74.00588035583496,40.71779411151555\n' +
+        '</coordinates>' +
+        '</LineString>' +
+        '</Placemark>' +
+        '</Document>\n' +
+        '</kml>\n';
 
-var mmap=new GMap2(document.getElementById("gmap_poly_map"));
+var mmap=new GMap2(document.getElementById("gmap_poly_map_widget"));
 
-// hard code a nyc zoom for now
-var lat = "40.728078";
-var lng = "-73.997040";
-var zoom = "14";
+// Get the center point and zoom of the group map.
+var MapSettings = Drupal.settings.gmap_poly_group_map;
 
-// mmap.setCenter(new GLatLng(lat,lng),zoom);
+var lat = MapSettings.lat;
+var lng = MapSettings.lng;
+var zoom = MapSettings.zoom;
 
-    mmap.setCenter(new GLatLng(40.728078,-73.997040),14);
-    mmap.addControl(new GLargeMapControl());
-    mmap.addControl(new GMapTypeControl());
+// change these into usable values
+zoom = parseInt(zoom);
+var center = new GLatLng(lat,lng)
 
-    var exml = new EGeoXml("exml", mmap, null);
-    exml.parseString(kmltest);
+// pass settings to the map.
+// mmap.setCenter(center, zoom);
+// hard code a nyc zoom for testing.
+mmap.setCenter(new GLatLng(40.728078,-73.997040),15);
+
+mmap.addControl(new GLargeMapControl());
+mmap.addControl(new GMapTypeControl());
+
+var exml = new EGeoXml("exml", mmap, null);
+exml.parseString(kmltest);
