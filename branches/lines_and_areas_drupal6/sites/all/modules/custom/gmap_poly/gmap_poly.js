17dee6405dc8bac60da180f024a5a7bd0e14739e
nat <nat@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2009-12-16 20:10:53 +0000

    Added support for dynamic div naming based on field settings. Added textarea to save coordinates in. Added listener to write coordinates to the textarea on line exit.

--- branches/lines_and_areas_drupal6/sites/all/modules/custom/gmap_poly/gmap_poly.js
+++ branches/lines_and_areas_drupal6/sites/all/modules/custom/gmap_poly/gmap_poly.js
@@ -12,7 +12,14 @@ var kmltest = '<?xml version="1.0" encoding="UTF-8"?>\n' +
         '</Document>\n' +
         '</kml>\n';
 
-var mmap=new GMap2(document.getElementById("gmap_poly_map_widget"));
+
+// Declare a few div ids that will be used later
+var MapDivId = Drupal.settings.gmap_poly_widget.mapDiv;
+var MapCoordId = Drupal.settings.gmap_poly_widget.coorDiv;
+var MapFieldId = Drupal.settings.gmap_poly_widget.fieldDiv;
+
+// create a new GMap instance
+var mmap=new GMap2(document.getElementById(MapDivId));
 
 // Get the center point and zoom of the group map.
 var MapSettings = Drupal.settings.gmap_poly_group_map;
@@ -30,19 +37,52 @@ mmap.setCenter(center, zoom);
 // hard code a nyc zoom for testing.
 //mmap.setCenter(new GLatLng(parseFloat(40.728078),parseFloat(-73.997040)),15);
 
+// add map controls
 mmap.addControl(new GLargeMapControl());
 mmap.addControl(new GMapTypeControl());
 
+//define the line
 var polyline = new GPolyline([
   new GLatLng(parseFloat(lat), parseFloat(lng)),
   new GLatLng(parseFloat(40.72085157020638), parseFloat(-73.99309158325195)),
   new GLatLng(parseFloat(40.728078), parseFloat(-74))
-], "#000000", 10);
+], "#000000", 5);
 
-
+// add the line to the map
 mmap.addOverlay(polyline);
+
+// turn on line drawing
 polyline.enableDrawing();
 
+// this listener populates the textfield with the "output" coordinates variable.
+polyLineUpdatedListener = GEvent.addListener(polyline,"lineupdated",function(){
+  var output = "";
+
+  // loop through the line vertices to grab and strip out the lat/lng coordinates
+  for(var i = 0; i < polyline.getVertexCount();i++){
+    var tmp = "";
+    // get an individual set of points
+    tmp += polyline.getVertex(i);
+    // clean some Google stuff out
+    tmp = tmp.replace("(","");
+    tmp = tmp.replace(")","");
+
+    var tmpLatLng = tmp.split(",");
+
+    // add a space between points
+    if(output != ""){
+      output += " ";
+    }
+    // build a set of coordinates
+    output +=  tmpLatLng[1] + "," + tmpLatLng[0];
+  }
+
+  // name the textarea
+  var textarea = document.getElementById(MapFieldId);
+
+  // write coordinates to the text area
+  textarea.value = output;
+});
 
 // var exml = new EGeoXml("exml", mmap, null);
 // exml.parseString(kmltest);
