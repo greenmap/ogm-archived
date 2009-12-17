5f1e9506792f181630c948eea68b75bdde793be4
nat <nat@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2009-12-17 21:12:51 +0000

    Moved global variables to the top of the file. Moved line even listeners into a new newpolyline function. Added more listeners to build out widget line interface. Added remove line function reset function.

--- branches/lines_and_areas_drupal6/sites/all/modules/custom/gmap_poly/gmap_poly.js
+++ branches/lines_and_areas_drupal6/sites/all/modules/custom/gmap_poly/gmap_poly.js
@@ -4,6 +4,10 @@ var MapDivId = Drupal.settings.gmap_poly_widget_0.mapDiv;
 var MapCoordId = Drupal.settings.gmap_poly_widget_0.coorDiv;
 var MapFieldId = Drupal.settings.gmap_poly_widget_0.fieldDiv;
 
+var polymouseOver = false;
+var polyMissing = false;
+var polyline;
+
 // create a new GMap instance
 var mmap=new GMap2(document.getElementById(MapDivId));
 
@@ -20,63 +24,89 @@ var center = new GLatLng(parseFloat(lat),parseFloat(lng));
 
 // pass settings to the map.
 mmap.setCenter(center, zoom);
-// hard code a nyc zoom for testing.
-//mmap.setCenter(new GLatLng(parseFloat(40.728078),parseFloat(-73.997040)),15);
 
 // add map controls
 mmap.addControl(new GLargeMapControl());
 mmap.addControl(new GMapTypeControl());
 
-//define the line
-var polyline = new GPolyline([], "#000000", 5);
+var polylineCoordinates = "";
 
-// add the line to the map
-mmap.addOverlay(polyline);
-
-// turn on line drawing
-polyline.enableDrawing();
-
-// this listener populates the textfield with the "output" coordinates variable.
-polyLineUpdatedListener = GEvent.addListener(polyline,"lineupdated",function(){
-  var output = "";
-
-  // loop through the line vertices to grab and strip out the lat/lng coordinates
-  for(var i = 0; i < polyline.getVertexCount();i++){
-    var tmp = "";
-    // get an individual set of points
-    tmp += polyline.getVertex(i);
-    // clean some Google stuff out
-    tmp = tmp.replace("(","");
-    tmp = tmp.replace(")","");
-
-    var tmpLatLng = tmp.split(",");
-
-    // add a space between points
-    if(output != ""){
-      output += " ";
-    }
-    // build a set of coordinates
-    output +=  tmpLatLng[1] + "," + tmpLatLng[0];
-  }
-
+function newPolyline(){
   // name the textarea
   var textarea = document.getElementById(MapFieldId);
+  textarea.value = polylineCoordinates;
 
-  // write coordinates to the text area
-  textarea.value = output;
+  //define the line
+  polyline = new GPolyline([], "#000000", 5);
+
+  // add the line to the map
+  mmap.addOverlay(polyline);
+
+  // turn on line drawing
+  polyline.enableDrawing();
+
+
+  // Mouseover the the poly line
+  polyMouseoverListener = GEvent.addListener(polyline, "mouseover", function() {
+    polymouseOver = true;
+    polyline.enableEditing(); // edit points are shown
+  });
+
+  // Mouse Out for the poly line
+  polyMouseoutListener = GEvent.addListener(polyline, "mouseout", function() {
+    polymouseOver = false;
+    polyline.disableEditing();
+  });
+
+  // Mouse Out for the poly line
+  polyCancelineListener = GEvent.addListener(polyline, "cancelline", function() {
+    if (polyline.getVertexCount() <= 1) {
+      polyMissing = true;
+    }
+
+  });
+
+  // this listener populates the textfield with the "output" coordinates variable.
+  polyLineUpdatedListener = GEvent.addListener(polyline,"lineupdated",function(){
+    var output = "";
+
+    // loop through the line vertices to grab and strip out the lat/lng coordinates
+    for(var i = 0; i < polyline.getVertexCount();i++){
+      var tmp = "";
+      // get an individual set of points
+      tmp += polyline.getVertex(i);
+      // clean some Google stuff out
+      tmp = tmp.replace("(","");
+      tmp = tmp.replace(")","");
+
+      var tmpLatLng = tmp.split(",");
+
+      // add a space between points
+      if(output != ""){
+        output += " ";
+      }
+      // build a set of coordinates
+      output +=  tmpLatLng[1] + "," + tmpLatLng[0];
+    }
+
+    // write coordinates to the text area
+    textarea.value = output;
+  });
+};
+
+newPolyline();
+
+
+
+
+// single left click on the map
+GEvent.addListener(mmap, "click",function() {
+  if (polyMissing) {
+    polyMissing = false;
+    newPolyline();
+  }
 });
 
-// Mouseover the the poly line
-polyMouseoverListener = GEvent.addListener(polyline, "mouseover", function() {
-  polymouseOver = true;
-  polyline.enableEditing(); // edit points are shown
-});
-
-// Mouse Out for the poly line
-polyMouseoutListener = GEvent.addListener(polyline, "mouseout", function() {
- polymouseOver = false;
- polyline.disableEditing(); // editpoints are hided
-});
 
 // single right click on the map
 GEvent.addListener(mmap, "singlerightclick",function(a,b,overlay) {
@@ -85,3 +115,10 @@ GEvent.addListener(mmap, "singlerightclick",function(a,b,overlay) {
     polyline.deleteVertex(overlay.index);
   }
 });
+
+
+function clearPoly() {
+  mmap.removeOverlay(polyline);
+  newPolyline();
+};
+
