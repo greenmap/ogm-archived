2c44ea270beabb753d5e0ff5afb5d5840d755803
nat <nat@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-01-14 22:38:35 +0000

    Updated to include areas.

--- branches/lines_and_areas_drupal6/sites/all/modules/custom/ogm_ol/ogm_ol.js
+++ branches/lines_and_areas_drupal6/sites/all/modules/custom/ogm_ol/ogm_ol.js
@@ -19,5 +19,23 @@ Drupal.gmap.addHandler('gmap',function(elem) {
       // add it to the map
       map.map.addOverlay(polyline);
     }
+
+    // loop through the area coordinates passed from the module for the map
+    for ( i = 0; i < Drupal.settings.ogm_ol_areas.length; i = i + 1 ) {
+      var coords = [];
+      var line = Drupal.settings.ogm_ol_areas[i];
+      // since there are multiple points in an area, loop through those,
+      // turn them into Google GLatLng objects, and add that to an array
+      for ( var j=line.length-1; j>=0; --j ) {
+        var latlon = new GLatLng(parseFloat(line[j][1]), parseFloat(line[j][0]));
+        coords.push(latlon);
+      }
+      // create the line instance
+      var polyarea = new GPolygon(coords, "#000000", 5);
+      // add it to the map
+      map.map.addOverlay(polyarea);
+    }
+
+
   });
 });
