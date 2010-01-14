66c37cb7c5f5b043ccf631a89a4760356e6146d1
nat <nat@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-01-14 23:57:23 +0000

    Passed nid of a line or an area from php to js in the module. Added click listenters to the javascript to pop up a bubble with information on the line or area. Changed dependencies in the info file.

--- sites/all/modules/custom/ogm_ol/ogm_ol.js
+++ sites/all/modules/custom/ogm_ol/ogm_ol.js
@@ -7,7 +7,8 @@ Drupal.gmap.addHandler('gmap',function(elem) {
     // loop through the line coordinates passed from the module for the map
     for ( i = 0; i < Drupal.settings.ogm_ol_lines.length; i = i + 1 ) {
       var coords = [];
-      var line = Drupal.settings.ogm_ol_lines[i];
+      var nid = Drupal.settings.ogm_ol_lines[i]['nid'];
+      var line = Drupal.settings.ogm_ol_lines[i]['coords'];
       // since there are multiple points in a line, loop through those,
       // turn them into Google GLatLng objects, and add that to an array
       for ( var j=line.length-1; j>=0; --j ) {
@@ -16,6 +17,8 @@ Drupal.gmap.addHandler('gmap',function(elem) {
       }
       // create the line instance
       var polyline = new GPolyline(coords, "#000000", 5);
+      // single left click on the line
+      GEvent.addListener(polyline, "click", OgmOlOnClick(nid, polyline.getBounds().getCenter()));
       // add it to the map
       map.map.addOverlay(polyline);
     }
@@ -23,7 +26,8 @@ Drupal.gmap.addHandler('gmap',function(elem) {
     // loop through the area coordinates passed from the module for the map
     for ( i = 0; i < Drupal.settings.ogm_ol_areas.length; i = i + 1 ) {
       var coords = [];
-      var line = Drupal.settings.ogm_ol_areas[i];
+      var nid = Drupal.settings.ogm_ol_areas[i]['nid'];
+      var line = Drupal.settings.ogm_ol_areas[i]['coords'];
       // since there are multiple points in an area, loop through those,
       // turn them into Google GLatLng objects, and add that to an array
       for ( var j=line.length-1; j>=0; --j ) {
@@ -32,10 +36,39 @@ Drupal.gmap.addHandler('gmap',function(elem) {
       }
       // create the line instance
       var polyarea = new GPolygon(coords, "#000000", 5);
+      // single left click on the area
+      GEvent.addListener(polyarea, "click", OgmOlOnClick(nid, polyarea.getBounds().getCenter()));
       // add it to the map
       map.map.addOverlay(polyarea);
     }
-
-
-  });
+ });
 });
+
+function OgmOlOnClick(nid, point) {
+  return function() {
+    var html = Drupal.makeReq(Drupal_base_path + Drupal_language + '/' + 'node/gmap_marker/getMiniBubble/' + nid,'');
+
+    html.onreadystatechange = function() {
+      if (html.readyState != 4) {
+        return;
+      }
+      if (html.status == 200) {// success
+        maxContentDiv = document.createElement('div');
+        // somewhere in here is a problem which results in two <html> tags
+        maxContentDiv.id = 'maxcontentdiv';
+        maxContentDiv.innerHTML = '<iframe frameborder="0" src="' + Drupal_base_path + Drupal_language + '/node/' + nid + '/simple" width="670" height="360"></    iframe>';
+        GlobalMap.openInfoWindowHtml(point, html.responseText,
+        {maxContent: maxContentDiv, maxTitle: ''});
+
+//         jQuery(function(){
+//           jQuery('.maximize').click(function() {
+//             var rel = jQuery(this).attr('rel');             GlobalMap.getInfoWindow().maximize();
+//           })
+//         });
+      }
+      else {
+        alert('Problems with the ajax (code 228)');
+      }
+    };
+  };
+};
