4dd22a878671ea28d163b00d865fab68d4df3ab4
nat <nat@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-02-05 21:07:21 +0000

    Tagging ogm_14

---
+++ tags/ogm_14/sites/all/modules/custom/ogm_ol/ogm_ol.js
@@ -0,0 +1,101 @@
+/**
+ * @file add js handlers for ogm lines and areas
+ */
+
+var allPolygons = [];
+
+
+Drupal.gmap.addHandler('gmap',function(elem) {
+  var map = this;
+  map.bind("ready", function() {
+    // loop through the line coordinates passed from the module for the map
+    for ( i = 0; i < Drupal.settings.ogm_ol_lines.length; i = i + 1 ) {
+      var coords = [];
+      var nid = Drupal.settings.ogm_ol_lines[i]['nid'];
+      var color = Drupal.settings.ogm_ol_lines[i]['color'];
+      var line = Drupal.settings.ogm_ol_lines[i]['coords'];
+      var opacity = Drupal.settings.ogm_ol_lines[i]['opacity'];
+      var tid = Drupal.settings.ogm_ol_lines[i]['tid'];
+      // since there are multiple points in a line, loop through those,
+      // turn them into Google GLatLng objects, and add that to an array
+      for ( var j=line.length-1; j>=0; --j ) {
+        var latlon = new GLatLng(parseFloat(line[j][1]), parseFloat(line[j][0]));
+        coords.push(latlon);
+      }
+      // create the line instance
+      var polyline = new GPolyline(coords, color, 5, opacity);
+      allPolygons.push([tid,polyline]);
+
+      // single left click on the line
+      GEvent.addListener(polyline, "click", OgmOlOnClick(nid, polyline.getBounds().getCenter()));
+      // add it to the map
+      map.map.addOverlay(polyline);
+    }
+
+    // loop through the area coordinates passed from the module for the map
+    for ( i = 0; i < Drupal.settings.ogm_ol_areas.length; i = i + 1 ) {
+      var coords = [];
+      var nid = Drupal.settings.ogm_ol_areas[i]['nid'];
+      var color = Drupal.settings.ogm_ol_areas[i]['color'];
+      var line = Drupal.settings.ogm_ol_areas[i]['coords'];
+      var opacity = Drupal.settings.ogm_ol_areas[i]['opactiy'];
+      var tid = Drupal.settings.ogm_ol_areas[i]['tid'];
+      // since there are multiple points in an area, loop through those,
+      // turn them into Google GLatLng objects, and add that to an array
+      for ( var j=line.length-1; j>=0; --j ) {
+        var latlon = new GLatLng(parseFloat(line[j][1]), parseFloat(line[j][0]));
+        coords.push(latlon);
+      }
+      // create the area instance
+      var polyarea = new GPolygon(coords, color, 3, opacity, color, opacity);
+      allPolygons.push([tid,polyarea]);
+      // single left click on the area
+      GEvent.addListener(polyarea, "click", OgmOlOnClick(nid, polyarea.getBounds().getCenter()));
+      // add it to the map
+      map.map.addOverlay(polyarea);
+    }
+ });
+});
+
+function OgmOlOnClick(nid, point) {
+  return function() {
+    var html = Drupal.makeReq(Drupal_base_path + Drupal_language + '/' + 'node/gmap_marker/getMiniBubble/' + nid,'');
+
+    html.onreadystatechange = function() {
+      if (html.readyState != 4) {
+        return;
+      }
+      if (html.status == 200) {// success
+        maxContentDiv = document.createElement('div');
+        // somewhere in here is a problem which results in two <html> tags
+        maxContentDiv.id = 'maxcontentdiv';
+        maxContentDiv.innerHTML = '<iframe frameborder="0" src="' + Drupal_base_path + Drupal_language + '/node/' + nid + '/simple" width="670" height="360"></    iframe>';
+        GlobalMap.openInfoWindowHtml(point, html.responseText,
+        {maxContent: maxContentDiv, maxTitle: ''});
+      }
+      else {
+        alert('Problems with the ajax (code 228)');
+      }
+    };
+  };
+};
+
+function ogmOlRemovePolies(map, tid) {
+  if (allPolygons) {
+    allPolygons.map(function(poly) {
+      if (poly[0] === tid) {
+        map.removeOverlay(poly[1]);
+      }
+    });
+  }
+}
+
+function ogmOlAddPolies(map, tid) {
+  if (allPolygons) {
+    allPolygons.map(function(poly) {
+      if (poly[0] === tid) {
+        map.addOverlay(poly[1]);
+      }
+    });
+  }
+}
