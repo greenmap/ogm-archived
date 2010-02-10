68919a264873eac67a445ba64a300457ea039045
mjgoins <mjgoins@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-02-10 23:55:12 +0000

    Tagging ogm_015

---
+++ tags/ogm_015/sites/all/modules/custom/widget/ajax.js
@@ -0,0 +1,52 @@
+/**
+ * @author Miikka Lammela
+ * @param url String url typicaly callback menu path processes informaiton example /dev/oqm_miikka/example
+ * @param params String POST params to url example: arg1=12&arg2=abc
+ * @param returnFunc String name of return handle function example: real name: Drupal.example => string: example
+ * @param returnArgs mixed Anything you might need to put to return handler function
+ */
+Drupal.makeRequest = function (url, params,returnFunc,returnArgs) {
+    var http_request = false;
+    if (window.XMLHttpRequest) { // Mozilla,...
+      http_request = new XMLHttpRequest();
+      if (http_request.overrideMimeType) {
+        http_request.overrideMimeType('text/xml');
+      }
+    } else if (window.ActiveXObject) { // Internet Explorer
+      try {
+        http_request = new ActiveXObject("Msxml2.XMLHTTP");
+      } catch (e) {
+         try {
+           http_request = new ActiveXObject("Microsoft.XMLHTTP");
+         } catch (e) {
+           alert('Failed to open XMLHTTP');
+         }
+      }
+    }
+
+    if (!http_request) {
+      alert('Failed to open XMLHTTP');
+        return false;
+    }
+   http_request.onreadystatechange = function() {
+   		if (http_request.readyState != 4) {
+			return;
+		}
+		// success
+		if (http_request.status == 200) {
+			// call return handler function
+			returnFunc = "Drupal." + returnFunc + "(http_request,returnArgs)";
+			eval(returnFunc);
+	  	} else {
+			// failed
+        	alert('Problems with the ajax (code 103):' + url);
+		}
+    };
+
+    http_request.open('POST', url, true);
+	//Send the proper header information along with the request
+	http_request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http_request.setRequestHeader("Content-length", params.length);
+	http_request.setRequestHeader("Connection", "close");
+    http_request.send(params);
+}
