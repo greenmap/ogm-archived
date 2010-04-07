d5fc825226f90c43b39ed4798f8038ce890216cf
mjgoins <mjgoins@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-04-07 21:02:28 +0000

    Tagging ogm_017.2

---
+++ tags/ogm_017.2/sites/all/themes/opengreenmap/comment.tpl.php
@@ -0,0 +1,45 @@
+<!--comment.tpl.php-->
+<div class="comment <?php print $comment_classes ." ". $zebra; ?>"><div class="comment-inner">
+  
+  <?php if ($unpublished) : ?>
+    <div class="unpublished"><?php print t('Unpublished'); ?></div>
+  <?php endif; ?> 
+  
+  <?php if ($links): ?>
+    <div class="links">
+      <?php print $links; ?>
+    </div>
+  <?php endif; ?> 
+  
+  <span class="submitted">
+    <?php print format_date($comment->timestamp, 'custom', 'j M Y'); ?> 
+    <?php
+    if($comment->homepage){
+      $commenter = l($comment->name,$comment->homepage);
+    } elseif ($comment->uid > 0) {
+      $commenter = l($comment->name, 'user/' . $comment->uid);
+    } else {
+      $commenter = $comment->name ? $comment->name : t('Anonymous');
+    }
+    
+    ?>
+    <?php print $commenter; ?> 
+    <?php print t('wrote'); ?>: 
+  </span>
+  
+  <span class="content">
+    <?php print $content; ?>
+  </span>
+  
+  
+  <?php if (!empty($new)): ?>
+    <div class="new"><?php print $new; ?></div>
+  <?php endif; ?>
+
+
+
+  <?php if ($picture) print $picture; ?>
+
+
+</div></div> <!-- /comment-inner, /comment -->
+<!--/comment.tpl.php-->
