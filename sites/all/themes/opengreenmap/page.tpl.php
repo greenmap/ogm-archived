6ad8177f42c9484928caa1d8a8c75189ee4de136
nat <nat@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-05-20 20:20:22 +0000

    CSS tweaks for IE placement, as well as moving of question mark div position. Addresses issue #2052.

--- sites/all/themes/opengreenmap/page.tpl.php
+++ sites/all/themes/opengreenmap/page.tpl.php
@@ -37,6 +37,14 @@ if ($_GET['isSimple']) {
       <link rel="stylesheet" href="<?php print $base_path . $subtheme_directory; ?>/ie.css" type="text/css">
     <?php endif; ?>
   <![endif]-->
+  <!--[if IE 8]>
+    <link rel="stylesheet" href="<?php print base_path() . path_to_theme(); ?>/ie8.css" type="text/css">
+    <?php if ($subtheme_directory && file_exists($subtheme_directory .'/ie.css')): ?>
+      <link rel=" stylesheet" href="<?php print $base_path . $subtheme_directory; ?>/ie.css" type="text/css">
+      <link rel="stylesheet" href="<?php print $base_path . $subtheme_directory; ?>/ie8.css" type="text/css">
+    <?php endif; ?>
+  <![endif]-->
+
   <?php print $scripts; ?>
 </head>
 
