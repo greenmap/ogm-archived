b17055bd00b8f6828a18c6c0e3f392146c71e86e
te <te@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-05-25 15:01:45 +0000

    add inlightbox argument for ogm front search

--- trunk/sites/all/themes/opengreenmap/page.tpl.php
+++ trunk/sites/all/themes/opengreenmap/page.tpl.php
@@ -17,6 +17,10 @@ if ($_GET['isSimple']) {
 	include('page-simple.tpl.php');
 	return;
 }
+if ($_GET['lightboxtheme']) {
+	include('page-inlightbox.tpl.php');
+	return;
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!--page.tpl.php-->
