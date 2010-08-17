f41040127dab7d31de39e7dcb3969f0b58bb18b9
gohai <gohai@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-08-17 15:56:41 +0000

    fix for hosts that use php's magic quotes

--- trunk/sites/all/modules/custom/ogm_api/test/proxy.php
+++ trunk/sites/all/modules/custom/ogm_api/test/proxy.php
@@ -2,22 +2,28 @@
 
 
 error_reporting(E_ALL);
+// API key is not used atm
 // $api_key = 'your api key goes here';
+// TODO: change to your host
+$host = 'http://staging.opengreenmap.org/';
 
 
 // setup the curl instance
 $c = curl_init();
 curl_setopt($c, CURLOPT_POST, true);
 curl_setopt($c, CURLOPT_RETURNTRANSFER, true);
-// TODO: change to your host
-curl_setopt($c, CURLOPT_URL, 'http://staging.opengreenmap.org/services/json');
+curl_setopt($c, CURLOPT_URL, $host.'services/json');
 
 // check if we got a method argument
 if (empty($_REQUEST['method'])) {
 	header($_SERVER['SERVER_PROTOCOL'].' 400 Bad Request');
 	die();
 } else {
-	$method = $_REQUEST['method'];
+	if (get_magic_quotes_gpc()) {
+		$method = stripslashes($_REQUEST['method']);
+	} else {
+		$method = $_REQUEST['method'];
+	}
 	unset($_REQUEST['method']);
 }
 
@@ -27,7 +33,11 @@ if (isset($api_key)) {
 	$data['api_key'] = '"'.$api_key.'"';
 }
 foreach ($_REQUEST as $key=>$val) {
-	$data[$key] = $val;
+	if (get_magic_quotes_gpc()) {
+		$data[$key] = stripslashes($val);
+	} else {
+		$data[$key] = $val;
+	}
 }
 curl_setopt($c, CURLOPT_POSTFIELDS, $data);
 
