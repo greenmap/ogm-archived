8d8e9fab9658cd15d05d9a2cd7c655395fa6f05e
mjgoins <mjgoins@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-09-24 17:30:52 +0000

    Revert mistaken upgrade of emfield to new major version

--- trunk/sites/all/modules/contrib/emfield/contrib/emvideo/emvideo.thumbnail-replacement.js
+++ trunk/sites/all/modules/contrib/emfield/contrib/emvideo/emvideo.thumbnail-replacement.js
@@ -1,23 +1,19 @@
-// $Id: emvideo.thumbnail-replacement.js,v 1.1.2.2 2009/09/01 20:35:24 aaron Exp $
+// $Id: emvideo.thumbnail-replacement.js,v 1.1.2.1 2009/02/26 03:35:54 aaron Exp $
 
 /**
  *  @file
  *  This will use jQuery AJAX to replace a thumbnail with its video version.
+ *
+ *  Drupal.settings.emvideo.thumbs will contain an array with the following:
+ *
  */
-Drupal.behaviors.emvideoThumbnailReplacement = function(context) {
-  // On load, add a span for the play button to any required thumbnails.
-  $('a.emvideo-thumbnail-replacement:not(.emvideo-thumbnail-replacement-processed)', context).addClass('emvideo-thumbnail-replacement-processed').each(function() {
-    if (Drupal.settings.emvideo.thumbnail_overlay) {
-      // Add the play button.
-      $(this).prepend("<span></span>");
-    }
-  });
 
-  // Inline videos will click through to the video.
-  $('a.emvideo-modal-emvideo:not(.emvideo-modal-emvideo-processed)', context).addClass('emvideo-modal-emvideo-processed').each(function() {
-    // When clicking the image, load the video with AJAX.
-    // Note that this only happens if this is not a lightbox.
-    $(this).click(function() {
+if (Drupal.jsEnabled) {
+  $(document).ready(function(){
+    if (Drupal.settings.emvideo.thumbnail_overlay) {
+      $(".emvideo-thumbnail-replacement").prepend("<span></span>");
+    }
+    $('.emvideo-thumbnail-replacement').click(function() {
       // 'this' won't point to the element when it's inside the ajax closures,
       // so we reference it using a variable.
       var element = this;
@@ -41,4 +37,4 @@ Drupal.behaviors.emvideoThumbnailReplacement = function(context) {
       return false;
     });
   });
-};
+}
