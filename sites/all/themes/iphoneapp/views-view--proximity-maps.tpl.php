db0d57815f90eb589ee081221b8f11c1ecf41f14
te <te@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-10-26 15:13:27 +0000

    new theme for iphone app

---
+++ sites/all/themes/iphoneapp/views-view--proximity-maps.tpl.php
@@ -0,0 +1,104 @@
+﻿<?php
+// $Id: views-view-field.tpl.php,v 1.1 2008/05/16 22:22:32 merlinofchaos Exp $
+ /**
+  * This template is used to print a single field in a view. It is not
+  * actually used in default Views, as this is registered as a theme
+  * function which has better performance. For single overrides, the
+  * template is perfectly okay.
+  *
+  * Variables available:
+  * - $view: The view object
+  * - $field: The field handler object that can process the input
+  * - $row: The raw SQL result that can be used
+  * - $output: The processed output that will normally be used.
+  *
+  * When fetching output from the $row, this construct should be used:
+  * $data = $row->{$field->field_alias}
+  *
+  * The above will guarantee that you'll always get the correct data,
+  * regardless of any changes in the aliasing that might happen if
+  * the view is modified.
+  */
+  
+/* *
+ * Here we take the vid (revision id) and get from that the primary term, and from that the Icon.
+ * Convoluted I know, but primary_term module doesn't have proper views support.
+ */ 
+?>
+
+
+<?php
+$lat = $_GET['distance']['latitude'];
+$lon = $_GET['distance']['longitude'];
+$_SESSION['ogmmobilelocation'] = array('lat' => $lat, 'lon' => $lon);
+echo $lat; echo $lon;
+?>
+
+<div class="view view-<?php print $css_name; ?> view-id-<?php print $name; ?> view-display-id-<?php print $display_id; ?> view-dom-id-<?php print $dom_id; ?>">
+  <?php if ($admin_links): ?>
+    <div class="views-admin-links views-hide">
+      <?php print $admin_links; ?>
+    </div>
+  <?php endif; ?>
+  <?php if ($header): ?>
+    <div class="view-header">
+      <?php print $header; ?>
+      Please select which map you would like to suggest your site to. It will then be reviewed by the mapmaker 
+    </div>
+  <?php endif; ?>
+
+  <?php if ($exposed): ?>
+    <div class="view-filters">
+      <?php print $exposed; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($attachment_before): ?>
+    <div class="attachment attachment-before">
+      <?php print $attachment_before; ?>
+    </div>
+  <?php endif; ?>
+ 
+ <style type="text/css">
+.tabs {
+display: none;
+}
+</style>
+
+  <?php if ($rows): ?>
+    <div class="view-content">
+      <?php print $rows; ?>
+    </div>
+  <?php elseif ($empty): ?>
+    <div class="view-empty">
+      <?php print $empty; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($pager): ?>
+    <?php print $pager; ?>
+  <?php endif; ?>
+
+  <?php if ($attachment_after): ?>
+    <div class="attachment attachment-after">
+      <?php print $attachment_after; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($more): ?>
+    <?php print $more; ?>
+  <?php endif; ?>
+
+  <?php if ($footer): ?>
+    <div class="view-footer">
+      <?php print $footer; ?>
+    </div>
+  <?php endif; ?>
+
+  <?php if ($feed_icon): ?>
+    <div class="feed-icon">
+      <?php print $feed_icon; ?>
+    </div>
+  <?php endif; ?>
+
+</div> <?php /* class view */ ?>
