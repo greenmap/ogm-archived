c542ca8e4040e61b4b1f454a3af0ee17d8320d08
te <te@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-12-03 20:41:59 +0000

    iphone tweaks

--- trunk/sites/all/themes/iphoneapp/node.tpl.php
+++ trunk/sites/all/themes/iphoneapp/node.tpl.php
@@ -47,7 +47,19 @@
  * @see template_preprocess()
  * @see template_preprocess_node()
  */
-?>
+?><script type="text/javascript">
+ var updateLayout = function() {
+  if (window.innerWidth != currentWidth) {
+    currentWidth = window.innerWidth;
+    var orient = (currentWidth == 320) ? "profile" : "landscape";
+    document.body.setAttribute("orient", orient);
+    window.scrollTo(0, 1);
+  }
+};
+
+iPhone.DomLoad(updateLayout);
+setInterval(updateLayout, 500);
+</script>
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"><div class="node-inner">
   <?php print $picture; ?>
 
