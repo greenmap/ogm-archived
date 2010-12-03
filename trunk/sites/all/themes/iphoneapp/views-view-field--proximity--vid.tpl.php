b959beaedcb91eae6a62218e3c64eb3727cc8b52
te <te@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-12-03 20:41:07 +0000

    iphone tweaks

--- trunk/sites/all/themes/iphoneapp/views-view-field--proximity--vid.tpl.php
+++ trunk/sites/all/themes/iphoneapp/views-view-field--proximity--vid.tpl.php
@@ -25,6 +25,19 @@
  * Convoluted I know, but primary_term module doesn't have proper views support.
  */ 
 ?>
+<script type="text/javascript">
+ var updateLayout = function() {
+  if (window.innerWidth != currentWidth) {
+    currentWidth = window.innerWidth;
+    var orient = (currentWidth == 320) ? "profile" : "landscape";
+    document.body.setAttribute("orient", orient);
+    window.scrollTo(0, 1);
+  }
+};
+
+iPhone.DomLoad(updateLayout);
+setInterval(updateLayout, 500);
+</script>
 <?php 
 $vid = $row->{$field->field_alias};
 $primary_term_tid = primary_term_get_term($vid);
