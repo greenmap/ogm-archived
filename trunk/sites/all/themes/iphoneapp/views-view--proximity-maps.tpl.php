757b7e893481b36b2b8e45ab05f8483ebcf6e307
te <te@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2010-12-03 20:41:17 +0000

    iphone tweaks

--- trunk/sites/all/themes/iphoneapp/views-view--proximity-maps.tpl.php
+++ trunk/sites/all/themes/iphoneapp/views-view--proximity-maps.tpl.php
@@ -26,7 +26,19 @@
  */ 
 ?>
 
+<script type="text/javascript">
+ var updateLayout = function() {
+  if (window.innerWidth != currentWidth) {
+    currentWidth = window.innerWidth;
+    var orient = (currentWidth == 320) ? "profile" : "landscape";
+    document.body.setAttribute("orient", orient);
+    window.scrollTo(0, 1);
+  }
+};
 
+iPhone.DomLoad(updateLayout);
+setInterval(updateLayout, 500);
+</script>
 <?php
 $lat = $_GET['distance']['latitude'];
 $lon = $_GET['distance']['longitude'];
