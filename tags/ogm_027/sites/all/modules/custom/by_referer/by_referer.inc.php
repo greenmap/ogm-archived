ee126f350fc41532cb5b553e93e860b65ca75174
thomasturnbull <thomasturnbull@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2011-11-03 22:21:50 +0000

    this is the completed version of the code for the massive module upgrade, we will be launching with this tomorrow

---
+++ tags/ogm_027/sites/all/modules/custom/by_referer/by_referer.inc.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ *   the key to the array must be '#pattern#' where pattern is the string to
+ *   match. See the manual for preg_replace for details. 
+ *
+ *   You can also use '/pattern/'
+ */
+$by_referer_config = array(
+  // key is a pattern to match in a drupal path
+    '#mobile/list#' => array(
+    // string 'referers' is always the key here
+    'referers' => array(
+      // key is pattern for referer, value is name of theme
+      '#greenmap.org/greenhouse/files/widget/wgn.html#' => 'widgetized','#greenmap.org/greenhouse/files/widget/wgngreen.html#' => 'widgetizedgreen','#/sites/all/themes/frontsearch/ogmfront.html#' => 'frontsearch'
+    ),
+  ),
+   '#^taxsearch#' => array(
+    // string 'referers' is always the key here
+    'referers' => array(
+      // key is pattern for referer, value is name of theme
+      '#greenmap.org/greenhouse/files/widget/wgn.html#' => 'widgetized','#greenmap.org/greenhouse/files/widget/wgngreen.html#' => 'widgetizedgreen','#greenmap.org/greenhouse/files/widget/ogmfront.html#' => 'frontsearch'
+
+    ),
+  ),
+  'DEBUG' => FALSE,
+);
