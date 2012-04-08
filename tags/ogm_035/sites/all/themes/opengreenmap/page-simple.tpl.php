929bc8b2f6c8a8cf653cd153fc71aad83f7f75c3
thomasturnbull <thomasturnbull@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2012-04-08 19:29:32 +0000

    new tag that includes changes to the ogm_api module (added stripslashes to deal with mobile problem) and updated the services module

---
+++ tags/ogm_035/sites/all/themes/opengreenmap/page-simple.tpl.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--page-simple.tpl.php-->
+<html xmlns="http://www.w3.org/1999/xhtml" lang="<?php print $language-language; ?>" xml:lang="<?php print $language-language; ?>">
+
+<head>
+  <title><?php print $head_title; ?></title>
+  <?php print $head; ?>
+  <?php print $styles; ?>
+  <script type="text/javascript">
+    var Drupal_base_path = '<?php print base_path()?>';
+    var Drupal_language = '<?php print $language->language ?>';
+  </script>
+
+  <!--[if IE]>
+    <link rel="stylesheet" href="<?php print $base_path . $directory; ?>/ie.css" type="text/css">
+    <?php if ($subtheme_directory && file_exists($subtheme_directory .'/ie.css')): ?>
+      <link rel="stylesheet" href="<?php print $base_path . $subtheme_directory; ?>/ie.css" type="text/css">
+    <?php endif; ?>
+  <![endif]-->
+  <?php print $scripts; ?>
+  
+   <?php
+	if($_GET['view']) {
+		echo '<script type="text/javascript" src="'.base_path().drupal_get_path('module','widget').'/map.js"></script>';
+	}
+  ?>
+  
+</head>
+
+<body class="not-front no-sidebars simple">
+<div id="page"><div id="page-inner">
+
+
+      <div id="content"><div id="content-inner">
+<?php
+/*while(list($k,$v) = each($_GET)){
+	echo $k . " " . $v . "|";
+}*/
+print($nid);
+?>
+
+        <div id="content-area">
+          <?php print $content; ?>
+        </div>
+
+
+
+      </div></div> <!-- /#content-inner, /#content -->
+    <?php if ($closure_region): ?>
+      <div id="closure-blocks"><?php print $closure_region; ?></div>
+    <?php endif; ?>
+
+    <?php print $closure; ?>
+</div></div>
+</body>
+</html>
+<!--/page-simple.tpl.php-->
