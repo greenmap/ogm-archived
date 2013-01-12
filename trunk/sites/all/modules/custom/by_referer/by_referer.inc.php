214721168fd64cf5c28e1ff337b19afd8b9f2740
te <te@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2013-01-12 18:12:24 +0000

    adding further theme switcher support for the new mobile application when coming from the beta url only

--- trunk/sites/all/modules/custom/by_referer/by_referer.inc.php
+++ trunk/sites/all/modules/custom/by_referer/by_referer.inc.php
@@ -30,7 +30,7 @@ $by_referer_config = array(
       '#markfielbig.com/files/web/#' => 'iphoneapp','#addsites/locate.html/#' => 'iphoneapp'
     ),
   ),
-     '#^mobile#' => array(
+     '#mobile#' => array(
     // string 'referers' is always the key here
     'referers' => array(
       // key is pattern for referer, value is name of theme
