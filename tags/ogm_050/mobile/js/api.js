37578088761bed55bd20d4908fce917ca6486b2b
thomasturnbull <thomasturnbull@edd8b5b1-d58a-4b2c-adfe-7df10e4ea40c>
2013-12-20 05:00:40 +0000

    Hanne's fixes and my fixes to typos in site template into a new tag - Thomas

---
+++ tags/ogm_050/mobile/js/api.js
@@ -0,0 +1,44 @@
+/**
+ * API functions to interact with GreenMaps API
+ **/
+window.GreenMaps = window.GreenMaps ? window.GreenMaps : {};
+if (!GreenMaps.GreenApi){
+  GreenMaps.GreenApi = new function() {
+    var BASEURL = "http://nogoogle:nogoogle@www.opengreenmap.org/api/";
+
+    /**
+     * Get the /page/th page of POIs within /search_distance/ of 
+     * /latitude/, /longitude/, and call /callback/ with the
+     * result from the server.
+     **/
+    this.GetProximity = function(lat,
+        lon,
+        distance,
+        units,
+        page,
+        callback) {
+      var reqData = {
+        "distance[latitude]": lat,
+        "distance[longitude]": lon,
+        "distance[search_distance]": distance,
+        "distance[search_units]": units,
+        "page": page
+      };
+      window.proximity = callback;
+      $.getJSON(BASEURL + "proximity?callback=?", reqData, callback);
+      console.log(reqData);
+    };
+
+    /**
+     * Get detailed data about one POI with id number /nid/ and 
+     * return to /callback/.
+     **/
+    this.GetFullData = function(nid, callback) {
+      var reqData = {
+        "nid": nid
+      };
+      window.site = callback;
+      $.getJSON(BASEURL + "site?callback=?", reqData, callback);
+    };
+  };
+}
